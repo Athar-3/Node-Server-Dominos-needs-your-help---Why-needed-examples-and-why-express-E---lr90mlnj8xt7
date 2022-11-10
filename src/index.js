fc96a3d0a3a54c2ff705529c62d2682fce0c27a0
Athar-3 <shaikhathar0007@gmail.com>
2022-11-10 09:35:45 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function handleServer(req, res) {
   const url = req.url;
   if (url === "/") {
     res.writeHead(404);
+    res.end();
   }
   if (url === "/welcome") {
     res.setHeader("Content-Type", "text/plain");
@@ -15,7 +16,7 @@ function handleServer(req, res) {
   if (url === "/contact") {
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
-    res.end(`phone: '18602100000',email: 'guestcaredominos@jublfood.com'`);
+    res.end(`{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}`);
   }
 }
 
