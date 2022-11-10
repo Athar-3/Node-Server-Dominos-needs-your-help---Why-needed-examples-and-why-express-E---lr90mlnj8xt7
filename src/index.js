46a6c403324d3b0dc3b846a5985f0c3379938e13
Athar-3 <shaikhathar0007@gmail.com>
2022-11-10 09:25:08 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(404);
+  }
+  if (url === "/welcome") {
+    res.setHeader("Content-Type", "text/plain");
+    res.writeHead(200);
+    res.end("Welcome to Dominos!");
+  }
+  if (url === "/contact") {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(`phone: '18602100000',email: 'guestcaredominos@jublfood.com'`);
+  }
 }
 
+httpServer.listen(8081, () => {});
 module.exports = httpServer;
